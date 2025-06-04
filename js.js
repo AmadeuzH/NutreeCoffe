8396dbdbace3426c6c4dc36da454733a43b3befb
AmadeuzH <120730713+AmadeuzH@users.noreply.github.com>
2025-06-04 16:50:57 +0700

    Update js.js

--- js.js
+++ js.js
@@ -7,6 +7,7 @@ let count = {
     mierebus : 0,
     miegoreng : 0,
     telur : 0,
+    basotahu : 0,
    
 
 };
@@ -19,6 +20,7 @@ const prices = {
   mierebus: 5000,
   miegoreng: 5000,
   telur: 3000,
+    basotahu : 10000,
 
     
 };
@@ -118,8 +120,8 @@ document.getElementById("input-nama-miegoreng").value = menumiegoreng;
 let menutelur = document.getElementById("menutelur").innerText;
 document.getElementById("input-nama-telur").value = menutelur;
 
-// let menubasotahu = document.getElementById("menubasotahu").innerText;
-// document.getElementById("input-nama-basotahu").value = menubasotahu;
+let menubasotahu = document.getElementById("menubasotahu").innerText;
+document.getElementById("input-nama-basotahu").value = menubasotahu;
     
 // function validateSelect() {
 //   const select = document.getElementById("mp");
